5b829a47180b20149e0d960471dece19f2aa379e
chanaya-ceuterick <chanaya.ceuterick@student.howest.be>
2025-01-26 15:53:07 +0100

    javascript united + language animations

--- Integration/src/js/main.js
+++ Integration/src/js/main.js
@@ -1,6 +1,198 @@
-import '../css/style.css'
-// import javascriptLogo from './javascript.svg'
+import '../css/style.css';
+gsap.registerPlugin(ScrollTrigger);
 
-import './_intro.js';
-import './_civil.js';
-import './_language.js';
+// intro 
+
+gsap.fromTo('#imgone', {
+    x: '-120%',
+    rotation: 30
+}, {
+    scrollTrigger: {
+        trigger: '#imgone',
+        start: 'top 60%',
+        end: 'top 30%',
+        scrub: true,
+    },
+    x: '0%',
+    rotation: 0,
+    duration: 1
+});
+
+gsap.fromTo('#imgtwo', {
+    x: '120%',
+    rotation: -30
+}, {
+    scrollTrigger: {
+        trigger: '#imgtwo',
+        start: 'top 60%',
+        end: 'top 30%',
+        scrub: true,
+    },
+    x: '0%',
+    rotation: 0,
+    duration: 1
+});
+
+gsap.fromTo('.c-intro__txt h2, .c-intro__txt p', {
+    color: 'transparent'
+}, {
+    scrollTrigger: {
+        trigger: '.c-intro__txt',
+        start: 'top 60%',
+        end: 'top 30%',
+        scrub: true,
+    },
+    color: '#FCFCFC',
+    duration: 1
+});
+
+gsap.fromTo('.u-highlight', {
+    color: 'transparent'
+}, {
+    scrollTrigger: {
+        trigger: '.c-intro__txt',
+        start: 'top 60%',
+        end: 'top 30%',
+        scrub: true,
+    },
+    color: '#CB4F4F',
+    duration: 1
+});
+
+// civil rights
+
+gsap.fromTo('#civil-img, #civil-txt', {
+    opacity: 0,
+    y: '50%',
+}, {
+    scrollTrigger: {
+        trigger: '#civil-txt',
+        start: 'top 90%',
+        end: 'top 10%',
+        scrub: true
+    },
+    opacity: 1,
+    y: '0%',
+    duration: 1
+});
+
+gsap.to('#boy, #girl', {
+    scale: 1.5,
+    rotation: 60,
+    repeat: -1,
+    yoyo: true,
+    duration: 1,
+    ease: 'power1.inOut',
+});
+
+// language
+gsap.fromTo('.c-language__imgone', {
+    x: '-150%',
+    rotation: 30
+}, {
+    scrollTrigger: {
+        trigger: '.c-language__imgone',
+        start: 'top 60%',
+        end: 'top 30%',
+        scrub: true,
+    },
+    x: '0%',
+    rotation: 10,
+    duration: 1
+});
+
+gsap.fromTo('.c-language__imgtwo', {
+    x: '-200%',
+    rotation: -30
+}, {
+    scrollTrigger: {
+        trigger: '.c-language__imgtwo',
+        start: 'top 60%',
+        end: 'top 30%',
+        scrub: true,
+    },
+    x: '0%',
+    rotation: -10,
+    duration: 1
+});
+
+// word animation
+
+function wrapWords(selector) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(element => {
+        const textNodes = Array.from(element.childNodes).filter(node => node.nodeType === Node.TEXT_NODE);
+        textNodes.forEach(node => {
+            const words = node.textContent.split(' ');
+            const wrappedWords = words.map(word => `<span class="c-word">${word}</span>`).join(' ');
+            const span = document.createElement('span');
+            span.innerHTML = wrappedWords;
+            element.replaceChild(span, node);
+        });
+    });
+}
+
+function textAnimationIntro() {
+    wrapWords('#text-intro');
+    const words = gsap.utils.toArray('.c-word');
+
+    gsap.fromTo(words, {
+        opacity: 0.3,
+    }, {
+        opacity: 1,
+        duration: 0.4,
+        stagger: 0.5,
+        scrollTrigger: {
+            trigger: '#text-intro',
+            start: 'top 60%',
+            end: 'top 10%',
+            scrub: true,
+            toggleActions: "play none none reverse"
+        }
+    });
+}
+
+function textAnimationCivil() {
+    wrapWords('#text');
+    const words = gsap.utils.toArray('.c-word');
+
+    gsap.fromTo(words, {
+        opacity: 0.3,
+    }, {
+        opacity: 1,
+        duration: 0.4,
+        stagger: 0.5,
+        scrollTrigger: {
+            trigger: '#text',
+            start: 'top 60%',
+            end: 'top 10%',
+            scrub: true,
+            toggleActions: "play none none reverse"
+        }
+    });
+}
+
+function textAnimationLanguage() {
+    wrapWords('#language-textone, #language-texttwo');
+    const words = gsap.utils.toArray('.c-word');
+
+    gsap.fromTo(words, {
+        opacity: 0.3,
+    }, {
+        opacity: 1,
+        duration: 0.4,
+        stagger: 0.5,
+        scrollTrigger: {
+            trigger: '#language-textone',
+            start: 'top 60%',
+            end: 'top 20%',
+            scrub: true,
+            toggleActions: "play none none reverse"
+        }
+    });
+}
+
+// Initialisatie van animaties
+textAnimationIntro();
+textAnimationCivil();
+textAnimationLanguage();
