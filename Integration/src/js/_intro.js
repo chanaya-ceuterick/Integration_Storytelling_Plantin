74a6ad8399cbbba119e5e9bfcbabe03a1117557c
chanaya-ceuterick <chanaya.ceuterick@student.howest.be>
2025-01-26 15:14:29 +0100

    animations scrolltrigger improved

--- Integration/src/js/_intro.js
+++ Integration/src/js/_intro.js
@@ -1,19 +1,25 @@
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.to('#imgone', {
+gsap.fromTo('#imgone', {
+    x: '-120%',
+    rotation: 30
+}, {
     scrollTrigger: {
         trigger: '#imgone',
         start: 'top 60%',
         end: 'top 30%',
         scrub: true,
-        // markers: true,
     },
     x: '0%',
     rotation: 0,
     duration: 1
 });
 
-gsap.to('#imgtwo', {
+
+gsap.fromTo('#imgtwo', {
+    x: '120%',
+    rotation: -30
+}, {
     scrollTrigger: {
         trigger: '#imgtwo',
         start: 'top 60%',
@@ -25,7 +31,10 @@ gsap.to('#imgtwo', {
     duration: 1
 });
 
-gsap.to('.c-intro__txt h2, .c-intro__txt p', {
+
+gsap.fromTo('.c-intro__txt h2, .c-intro__txt p', {
+    color: 'transparent'
+}, {
     scrollTrigger: {
         trigger: '.c-intro__txt',
         start: 'top 60%',
@@ -36,7 +45,10 @@ gsap.to('.c-intro__txt h2, .c-intro__txt p', {
     duration: 1
 });
 
-gsap.to('.u-highlight', {
+
+gsap.fromTo('.u-highlight', {
+    color: 'transparent'
+}, {
     scrollTrigger: {
         trigger: '.c-intro__txt',
         start: 'top 60%',
@@ -45,4 +57,37 @@ gsap.to('.u-highlight', {
     },
     color: '#CB4F4F',
     duration: 1
+});
+
+function wrapWords(selector) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach(element => {
+        const textNodes = Array.from(element.childNodes).filter(node => node.nodeType === Node.TEXT_NODE);
+        textNodes.forEach(node => {
+            const words = node.textContent.split(' ');
+            const wrappedWords = words.map(word => `<span class="c-word">${word}</span>`).join(' ');
+            const span = document.createElement('span');
+            span.innerHTML = wrappedWords;
+            element.replaceChild(span, node);
+        });
+    });
+}
+
+wrapWords('#text-intro');
+const words = gsap.utils.toArray('.c-word');
+
+
+gsap.fromTo(words, {
+    opacity: 0.3,
+}, {
+    opacity: 1,
+    duration: 0.4,
+    stagger: 0.5,
+    scrollTrigger: {
+        trigger: '#text-intro',
+        start: 'top 60%',
+        end: 'top 10%',
+        scrub: true,
+        toggleActions: "play none none reverse"
+    }
 });
--- Integration/src/js/main.js
+++ Integration/src/js/main.js
@@ -2,3 +2,4 @@ import '../css/style.css'
 // import javascriptLogo from './javascript.svg'
 
 import './_intro.js';
+import './_civil.js';
