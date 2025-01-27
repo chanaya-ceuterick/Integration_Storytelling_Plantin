3fe4a8370388b2c5a2ab4c56361a68f18e0e36a6
chanaya-ceuterick <chanaya.ceuterick@student.howest.be>
2025-01-27 22:58:54 +0100

    daughters responsive

--- Integration/vite.config.js
+++ Integration/vite.config.js
@@ -1,4 +1,8 @@
 export default {
     // config options
     base: '/Integration_Storytelling_Plantin/', // Replace <repository-name> with your GitHub repository name
+    server: {
+        host: '0.0.0.0', // Dit maakt de server bereikbaar vanaf andere apparaten op hetzelfde netwerk
+        port: 3000, // Dit is de poort waarop de server draait (je kunt deze aanpassen)
+    }
 }
